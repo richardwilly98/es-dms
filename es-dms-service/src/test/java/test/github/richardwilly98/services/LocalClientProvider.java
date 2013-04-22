b2d92e36b818e65f73e63eb8d9ef2c9f52878e20
Richard Louapre <richard.louapre@gmail.com>
2013-04-22 16:44:22 -0400

    Add mapper-attachments in local node for tests

--- es-dms-service/src/test/java/test/github/richardwilly98/services/LocalClientProvider.java
+++ es-dms-service/src/test/java/test/github/richardwilly98/services/LocalClientProvider.java
@@ -3,9 +3,16 @@ package test.github.richardwilly98.services;
 import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
+import java.io.IOException;
+
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.collect.Tuple;
+import org.elasticsearch.common.io.FileSystemUtils;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.env.Environment;
 import org.elasticsearch.node.Node;
+import org.elasticsearch.node.internal.InternalSettingsPerparer;
+import org.elasticsearch.plugins.PluginManager;
 
 import com.google.inject.Provider;
 
@@ -19,28 +26,28 @@ public class LocalClientProvider implements Provider<Client> {
 	@Override
 	public Client get() {
 		Settings settings = getSettings();
-//		Tuple<Settings, Environment> initialSettings = InternalSettingsPerparer
-//				.prepareSettings(settings, true);
-//		PluginManager pluginManager = new PluginManager(
-//				initialSettings.v2(), null);
-//
-//		if (!initialSettings.v2().configFile().exists()) {
-//			FileSystemUtils.mkdirs(initialSettings.v2().configFile());
-//		}
-//
-//		if (!initialSettings.v2().logsFile().exists()) {
-//			FileSystemUtils.mkdirs(initialSettings.v2().logsFile());
-//		}
-//
-//		if (!initialSettings.v2().pluginsFile().exists()) {
-//			FileSystemUtils.mkdirs(initialSettings.v2().pluginsFile());
-//			try {
-//				pluginManager.downloadAndExtract(
-//						settings.get("plugins.mapper-attachments"), false);
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
+		Tuple<Settings, Environment> initialSettings = InternalSettingsPerparer
+				.prepareSettings(settings, true);
+		PluginManager pluginManager = new PluginManager(
+				initialSettings.v2(), null);
+
+		if (!initialSettings.v2().configFile().exists()) {
+			FileSystemUtils.mkdirs(initialSettings.v2().configFile());
+		}
+
+		if (!initialSettings.v2().logsFile().exists()) {
+			FileSystemUtils.mkdirs(initialSettings.v2().logsFile());
+		}
+
+		if (!initialSettings.v2().pluginsFile().exists()) {
+			FileSystemUtils.mkdirs(initialSettings.v2().pluginsFile());
+			try {
+				pluginManager.downloadAndExtract(
+						settings.get("plugins.mapper-attachments"), false);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		Node node = nodeBuilder().local(true).settings(settings).node();
 		Client client = node.client();
 		return client;
