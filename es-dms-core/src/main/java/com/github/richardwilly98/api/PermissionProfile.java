607777d23c916a041014e8cac29eeb1da87be16b
Danilo Sandron <danilo.sandron@gmail.com>
2013-04-30 01:55:27 +0700

    Deleted PermissionProfile

--- es-dms-core/src/main/java/com/github/richardwilly98/api/PermissionProfile.java
+++
@@ -1,9 +0,0 @@
-package com.github.richardwilly98.api;
-
-import java.util.List;
-
-public class PermissionProfile extends ItemBase{
-	
-	private static final long serialVersionUID = 1L;
-	List<String> securedproperties;
-}
