056beb757f325b12d869d7ab9aa3ba59a8a6a94f
Danilo Sandron <danilo.sandron@gmail.com>
2013-04-30 07:55:16 +0700

    RoleProvider tests

--- es-dms-service/src/test/java/test/github/richardwilly98/services/RoleProviderTest.java
+++ es-dms-service/src/test/java/test/github/richardwilly98/services/RoleProviderTest.java
@@ -6,9 +6,16 @@ import java.util.Map;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import test.github.richardwilly98.inject.ProviderModule;
+
 import com.github.richardwilly98.api.Permission;
 import com.github.richardwilly98.api.Role;
 import com.github.richardwilly98.api.services.PermissionService;
+import com.github.richardwilly98.api.services.RoleService;
+import com.github.richardwilly98.services.PermissionProvider;
+import com.github.richardwilly98.services.RoleProvider;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 public class RoleProviderTest extends ProviderTestBase {
 
@@ -24,9 +31,13 @@ public class RoleProviderTest extends ProviderTestBase {
 		Assert.assertEquals(description, role.getDescription());
 		log.trace("role disabled: " + role.isDisabled());
 		Assert.assertEquals(disabled, role.isDisabled());
-//		Assert.assertEquals(permissions, role.getPermissions());
 
 		return role.getId();
+	}
+	
+	protected RoleService getRoleProvider() {
+		Injector injector = Guice.createInjector(new ProviderModule());
+		return injector.getInstance(RoleProvider.class);
 	}
 
 	@Test
@@ -60,6 +71,9 @@ public class RoleProviderTest extends ProviderTestBase {
 		permissions.put("group:remove", createPermission("group:remove", "group:remove", true));
 		permissions.put("role:add", createPermission("role:add", "role:add", true));
 		permissions.put("role:remove", createPermission("role:remove", "role:remove", true));
+		testCreateRole("Editor", "Editor", permissions, false);
+		log.info("Editor permissions count: " + permissions.size());
+		Assert.assertEquals(permissions.size(), 18);
 		permissions.put("milestone:add", createPermission("milestone:add", "milestone:add", true));
 		permissions.put("milestone:remove", createPermission("milestone:remove", "milestone:remove", true));
 		permissions.put("task:assign", createPermission("task:assign", "task:assign", true));
@@ -70,21 +84,41 @@ public class RoleProviderTest extends ProviderTestBase {
 		log.info("Start testCreateRole completed!!");
 	}
 	
-//	@Test
-//	public void testFindUser() throws Throwable {
-//		log.info("Start testFindUser");
-//		
-//		UserService provider = getUserProvider();
-//		User user = provider.get("Richard");
-//		
-//		Assert.assertNotNull(user);
-//		if (!(user == null) )log.info("User found: " + user.getName());
-//		
-//		user = provider.get("Danilo");
-//		
-//		//Assert.assertNotNull(user);
-//		if (!(user == null))log.info("User found: " + user.getName());
-//	}
+	@Test
+	public void testFindRole() throws Throwable {
+		log.info("Start testFindRole");
+		
+		Map<String, Permission> permissions = new HashMap<String, Permission>();
+		
+		permissions.put("profile:read", createPermission("profile:read", "profile:read", true));
+		permissions.put("content:read", createPermission("content:read", "content:read", true));
+		permissions.put("annotation:read", createPermission("annotation:read", "annotation:read", true));
+		permissions.put("annotation:write", createPermission("annotation:write", "annotation:write", true));
+		permissions.put("comment:read", createPermission("comment:read", "comment:read", true));
+		permissions.put("comment:write", createPermission("comment:write", "comment:write", true));
+		permissions.put("content:todelete", createPermission("content:todelete", "content:todelete", true));
+		testCreateRole("Proof-Reader", "reader", permissions, false);
+		log.info("Proof-Reader permissions count: " + permissions.size());
+		
+		RoleService provider = getRoleProvider();
+		Role role = provider.get("Proof-Reader");
+		
+		Assert.assertNotNull(role);
+		if (!(role == null) )log.info("Role found: " + role.getName());
+		
+		permissions.put("profile:write", createPermission("profile:write", "profile:write", true));
+		permissions.put("content:write", createPermission("content:write", "content:write", true));
+		permissions.put("content:add", createPermission("content:add", "content:add", true));
+		permissions.put("content:remove", createPermission("content:remove", "content:remove", true));
+		permissions.put("profile:todelete", createPermission("profile:todelete", "profile:todelete", true));
+		testCreateRole("Writer", "writer", permissions, false);
+		log.info("Writer permissions count: " + permissions.size());
+		
+		role = provider.get("Writer");
+		
+		Assert.assertNotNull(role);
+		if (!(role == null))log.info("Role found: " + role.getName());
+	}
 //	
 //	@Test
 //	public void testAddUserRole() throws Throwable {
