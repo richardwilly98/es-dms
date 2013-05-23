5e5b5bc3d7865a0678a359c67241d057ae464295
Richard Louapre <richard.louapre@gmail.com>
2013-05-23 10:40:51 -0400

    Refactorize ES Client

--- es-dms-service/src/main/java/com/github/richardwilly98/inject/ClientProvider.java
+++ es-dms-service/src/main/java/com/github/richardwilly98/inject/ClientProvider.java
@@ -2,6 +2,7 @@ package com.github.richardwilly98.inject;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.network.NetworkUtils;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 
 import com.google.inject.Provider;
@@ -10,9 +11,11 @@ public class ClientProvider implements Provider<Client> {
 
 	@Override
 	public Client get() {
-		Client client = new TransportClient()
-		.addTransportAddress(new InetSocketTransportAddress(
-				"localhost", 9300));
+//		Builder builder = ImmutableSettings.settingsBuilder().loadFromClasspath("elasticsearch.yml");
+//		TransportClient client = new TransportClient(builder.build());
+		TransportClient client = new TransportClient();
+		client.addTransportAddress(new InetSocketTransportAddress(
+				NetworkUtils.getLocalAddress().getHostName(), 9300));
 		return client;
 	}
 
