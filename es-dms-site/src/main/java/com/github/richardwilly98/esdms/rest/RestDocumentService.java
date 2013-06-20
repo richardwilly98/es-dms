691d2ce88b79aaef32aebc1b7ce66c69a452ca51
Richard Louapre <richard.louapre@gmail.com>
2013-06-20 07:06:46 -0400

    Fix upload must return HTTP 201

--- es-dms-site/src/main/java/com/github/richardwilly98/esdms/rest/RestDocumentService.java
+++ es-dms-site/src/main/java/com/github/richardwilly98/esdms/rest/RestDocumentService.java
@@ -22,7 +22,6 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -238,7 +237,7 @@ public class RestDocumentService extends RestServiceBase<Document> {
 			Document document = new DocumentImpl.Builder().versions(new HashSet<Version>()).name(name).attributes(attributes).roles(null).build();
 			document = service.create(document);
 			Response response = Response
-					.ok(
+					.created(
 							getItemUri(document)).build();
 			Version version = new VersionImpl.Builder()
 			.documentId(document.getId()).current(true)
