d99226316ef833dbf05b63f7653b8f889a1d1459
Richard Louapre <richard.louapre@gmail.com>
2013-06-27 12:33:38 -0400

    Add license headers

--- es-dms-site/src/main/java/com/github/richardwilly98/esdms/web/RestGuiceServletConfig.java
+++ es-dms-site/src/main/java/com/github/richardwilly98/esdms/web/RestGuiceServletConfig.java
@@ -1,5 +1,32 @@
 package com.github.richardwilly98.esdms.web;
 
+/*
+ * #%L
+ * es-dms-site
+ * %%
+ * Copyright (C) 2013 es-dms
+ * %%
+ * Copyright 2012-2013 Richard Louapre
+ * 
+ * This file is part of ES-DMS.
+ * 
+ * The current version of ES-DMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * ES-DMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 
