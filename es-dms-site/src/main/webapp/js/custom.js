fbb404738b44bd418cf16880d12f8ddc1cf10746
Danilo Sandron <danilo.sandron@gmail.com>
2013-07-01 18:26:18 +0700

    cleanup

--- es-dms-site/src/main/webapp/js/custom.js
+++ es-dms-site/src/main/webapp/js/custom.js
@@ -1,19 +1,19 @@
-$(window).load(function(){
-	
-$('.popover-markup>.trigger').popover({ 
-	animation: true,
-	placement: 'bottom',
-    html : true,
-    title: function() {
-      return $(this).parent().find('.head').html();
-    },
-    content: function() {
-      return $(this).parent().find('.content').html();
-    }
-	});
-});
-
-$(window).click(function(){
-	alert("Closing");
-	$('.popover-markup>.btn').popover('hide');
-});
+//$(window).load(function(){
+//	alert("Hi loading popup");
+//$('.popover-markup>.trigger').popover({ 
+//	animation: true,
+//	placement: 'bottom',
+//    html : true,
+//    title: function() {
+//      return $(this).parent().find('.head').html();
+//    },
+//    content: function() {
+//      return $(this).parent().find('.content').html();
+//    }
+//	});
+//});
+//
+//$(window).click(function(){
+//	alert("Closing");
+//	$('.popover-markup>.btn').popover('hide');
+//});
