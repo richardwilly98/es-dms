21da45d72fcde6f60aa5cfd50332916c3880cba4
Richard Louapre <richard.louapre@gmail.com>
2013-09-28 11:04:46 -0400

    Allow role to be selected from user form

--- es-dms-site/yo/app/scripts/controllers/userEdit.js
+++ es-dms-site/yo/app/scripts/controllers/userEdit.js
@@ -1,6 +1,7 @@
 'use strict';
 
-esDmsSiteApp.controller('UserEditCtrl', ['$scope', '$rootScope', '$http', 'userService', function ($scope, $rootScope, $http, userService) {
+esDmsSiteApp.controller('UserEditCtrl', ['$log', '$scope', '$rootScope', '$http', 'userService', 'roleService',
+	function ($log, $scope, $rootScope, $http, userService, roleService) {
 	$scope.user = null;
 	$scope.newUser = false;
 	$scope.uid = '';
@@ -9,19 +10,40 @@ esDmsSiteApp.controller('UserEditCtrl', ['$scope', '$rootScope', '$http', 'userS
 	$scope.pw2 = '';
 	$scope.pwError = false;
 	$scope.incomplete = false;
+	$scope.selectedRoles = [];
+	$scope.roles = [];
 
 	$rootScope.$on('user:edit', function() {
+		$log.log('on user:edit');
 		var editUser = userService.currentUser();
 		if (editUser.id) {
 			$scope.user = editUser;
+			$log.log('user.roles: ' + JSON.stringify($scope.user.roles));
+			//$scope.selectedRoles = $scope.user.roles;
 			$scope.newUser = false;
 		} else {
 			$scope.newUser = true;
 			$scope.incomplete = true;
-			$scope.user = {};
+			$scope.user = {roles: []};
 			$scope.pw1 = '';
 			$scope.pw2 = '';
 		}
+		roleService.search('*', function(result) {
+			$scope.roles = result.items;
+			if (!$scope.newUser) {
+				// TODO: Must be optimized.
+				// TODO: Check the reason orderBy filter does not work. Items not selected.
+				_.each($scope.roles, function(role) {
+					_.find($scope.user.roles, function(item) {
+						if (item != null && role.id == item.id) {
+							$scope.selectedRoles.push(role);
+						}
+					})
+				});
+				$log.log('$scope.selectedRoles: ' + JSON.stringify($scope.selectedRoles));
+			}
+			$log.log('roles: ' + JSON.stringify($scope.roles));
+		});
     $scope.shouldBeOpen = true;
 	});
 	
@@ -38,6 +60,8 @@ esDmsSiteApp.controller('UserEditCtrl', ['$scope', '$rootScope', '$http', 'userS
 		if ($scope.newUser) {
 			$scope.user.password = $scope.pw1;
 		}
+		$log.log('About to save user: ' + JSON.stringify($scope.user));
+		$scope.user.roles = $scope.selectedRoles;
 		userService.save($scope.user);
     $scope.shouldBeOpen = false;
 	};
