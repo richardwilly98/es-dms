5da26d446ef44edea3701e884ebc171546094ba9
Richard Louapre <richard.louapre@gmail.com>
2013-09-30 19:43:01 -0400

    Hide Test menu (for the demo)

--- es-dms-site/yo/app/scripts/controllers/navbar.js
+++ es-dms-site/yo/app/scripts/controllers/navbar.js
@@ -8,8 +8,8 @@ esDmsSiteApp.controller('NavbarCtrl', ['$scope', function ($scope) {
 	$scope.tabs = [
 		{ 'view': '/index.documents-search', title: 'Search' },
 		/*{ 'view': '/my-documents-view', title: 'My documents' },*/
-		{ 'view': '/index.documents-edit', title: 'Edit' },
-    { 'view': '/index.test-accordion', title: 'Test' }/*,
+		{ 'view': '/index.documents-edit', title: 'Upload' },
+    /*{ 'view': '/index.test-accordion', title: 'Test' },
     { 'view': '/view2', title: 'View #2' },
     { 'view': '/view3', title: 'Test View'},
     { 'view': '/view4', title: 'View #4' }*/
