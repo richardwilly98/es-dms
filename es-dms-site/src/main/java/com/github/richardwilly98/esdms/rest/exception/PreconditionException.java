9a68e04078209d976a00118e879d20ff1a27d057
Richard Louapre <richard.louapre@gmail.com>
2013-09-30 19:40:10 -0400

    Throw correct exception for delete action

--- es-dms-site/src/main/java/com/github/richardwilly98/esdms/rest/exception/PreconditionException.java
+++ es-dms-site/src/main/java/com/github/richardwilly98/esdms/rest/exception/PreconditionException.java
@@ -27,13 +27,13 @@ package com.github.richardwilly98.esdms.rest.exception;
  */
 
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 @SuppressWarnings("serial")
 public class PreconditionException extends WebApplicationException {
 
     public PreconditionException(String message) {
-	super(Response.status(Response.Status.BAD_REQUEST).entity(message).type(MediaType.TEXT_PLAIN).build());
+	super(Response.status(Status.PRECONDITION_FAILED).entity(message).build());
     }
 }
