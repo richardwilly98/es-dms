a7204a59f93a1c5e9b91e2c6369098f115d929f9
Richard Louapre <richard.louapre@gmail.com>
2013-10-07 10:44:43 -0400

    Prevent duplicated object to be created
    - Add unit test.

--- es-dms-core/src/main/java/com/github/richardwilly98/esdms/api/Annotation.java
+++ es-dms-core/src/main/java/com/github/richardwilly98/esdms/api/Annotation.java
@@ -27,8 +27,4 @@ package com.github.richardwilly98.esdms.api;
  */
 
 public interface Annotation extends SecuredItem {
-
-    // SecuredItem parent;
-    // Object Content;
-
 }
--- es-dms-site/src/test/java/test/github/richardwilly98/esdms/rest/TestRestUserService.java
+++ es-dms-site/src/test/java/test/github/richardwilly98/esdms/rest/TestRestUserService.java
@@ -26,6 +26,9 @@ package test.github.richardwilly98.esdms.rest;
  * #L%
  */
 
+import java.net.URI;
+
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -49,63 +52,89 @@ import com.google.common.collect.ImmutableSet;
 public class TestRestUserService extends GuiceAndJettyTestBase<UserImpl> {
 
     public TestRestUserService() throws Exception {
-	super();
+        super();
     }
 
     private Role getRole(String id) throws Throwable {
-	Response response = target().path(RestRoleService.ROLES_PATH).path(id).request(MediaType.APPLICATION_JSON).cookie(adminCookie)
-	        .accept(MediaType.APPLICATION_JSON).get();
-	log.debug(String.format("status: %s", response.getStatus()));
-	if (response.getStatus() == Status.OK.getStatusCode()) {
-	    return response.readEntity(Role.class);
-	}
-	return null;
+        Response response = target().path(RestRoleService.ROLES_PATH).path(id).request(MediaType.APPLICATION_JSON).cookie(adminCookie)
+                .accept(MediaType.APPLICATION_JSON).get();
+        log.debug(String.format("status: %s", response.getStatus()));
+        if (response.getStatus() == Status.OK.getStatusCode()) {
+            return response.readEntity(Role.class);
+        }
+        return null;
     }
 
     @Test
     public void testGetUsers() throws Throwable {
-	log.debug("*** testGetUsers ***");
-	try {
-	    Response response;
-	    log.debug("Resource: " + target());
-	    response = target().path(RestUserService.USERS_PATH).path(RestItemBaseService.SEARCH_PATH)
-		    .path(UserService.DEFAULT_ADMIN_LOGIN).request(MediaType.APPLICATION_JSON).cookie(adminCookie).get();
-	    log.debug("status: " + response.getStatus());
-	    Assert.assertTrue(response.getStatus() == Status.OK.getStatusCode());
-	    SearchResult<User> users = response.readEntity(new GenericType<SearchResult<User>>() {
-	    });
-	    Assert.assertNotNull(users);
-	    Assert.assertTrue(users.getTotalHits() >= 1);
-	} catch (Throwable t) {
-	    log.error("testGetUsers fail", t);
-	    Assert.fail();
-	}
+        log.debug("*** testGetUsers ***");
+        try {
+            Response response;
+            log.debug("Resource: " + target());
+            response = target().path(RestUserService.USERS_PATH).path(RestItemBaseService.SEARCH_PATH)
+                    .path(UserService.DEFAULT_ADMIN_LOGIN).request(MediaType.APPLICATION_JSON).cookie(adminCookie).get();
+            log.debug("status: " + response.getStatus());
+            Assert.assertTrue(response.getStatus() == Status.OK.getStatusCode());
+            SearchResult<User> users = response.readEntity(new GenericType<SearchResult<User>>() {
+            });
+            Assert.assertNotNull(users);
+            Assert.assertTrue(users.getTotalHits() >= 1);
+        } catch (Throwable t) {
+            log.error("testGetUsers fail", t);
+            Assert.fail();
+        }
     }
 
     @Test
     public void testCreateRetrieveDeleteUpdate() throws Throwable {
-	log.debug("*** testCreateRetrieveDeleteUpdate ***");
-	try {
-	    String password = "secret1";
-	    String login = "user-" + System.currentTimeMillis();
-	    User user1 = createUser(login, password, ImmutableSet.of(RoleService.DefaultRoles.WRITER.getRole()));
-	    Assert.assertNotNull(user1);
-	    Assert.assertNotNull(user1.getRoles());
-	    Role writerRole = getRole(RoleService.DefaultRoles.WRITER.getRole().getId());
-	    Assert.assertTrue(user1.getRoles().contains(writerRole));
-	    UserImpl user2 = get(user1.getId(), UserImpl.class, RestUserService.USERS_PATH);
-	    Assert.assertEquals(user1.getName(), user2.getName());
-	    String newName = "user-2-" + System.currentTimeMillis();
-	    user2.setName(newName);
-	    UserImpl user3 = update(user2, UserImpl.class, RestUserService.USERS_PATH);
-	    Assert.assertEquals(newName, user3.getName());
-	    delete(user1.getId(), RestUserService.USERS_PATH);
-	    user2 = get(user1.getId(), UserImpl.class, RestUserService.USERS_PATH);
-	    Assert.assertNull(user2);
-	} catch (Throwable t) {
-	    log.error("testCreateRetrieveDeleteUpdate fail", t);
-	    Assert.fail();
-	}
+        log.debug("*** testCreateRetrieveDeleteUpdate ***");
+        try {
+            String password = "secret1";
+            String login = "user-" + System.currentTimeMillis();
+            User user1 = createUser(login, password, ImmutableSet.of(RoleService.DefaultRoles.WRITER.getRole()));
+            Assert.assertNotNull(user1);
+            Assert.assertNotNull(user1.getRoles());
+            Role writerRole = getRole(RoleService.DefaultRoles.WRITER.getRole().getId());
+            Assert.assertTrue(user1.getRoles().contains(writerRole));
+            UserImpl user2 = get(user1.getId(), UserImpl.class, RestUserService.USERS_PATH);
+            Assert.assertEquals(user1.getName(), user2.getName());
+            String newName = "user-2-" + System.currentTimeMillis();
+            user2.setName(newName);
+            UserImpl user3 = update(user2, UserImpl.class, RestUserService.USERS_PATH);
+            Assert.assertEquals(newName, user3.getName());
+            delete(user1.getId(), RestUserService.USERS_PATH);
+            user2 = get(user1.getId(), UserImpl.class, RestUserService.USERS_PATH);
+            Assert.assertNull(user2);
+        } catch (Throwable t) {
+            log.error("testCreateRetrieveDeleteUpdate fail", t);
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void testAvoidDuplicate() throws Throwable {
+        log.debug("*** testAvoidDuplicate ***");
+        try {
+            String password = "11111111";
+            String login = "user-" + System.currentTimeMillis();
+            User user = new UserImpl.Builder().id(login).name(login).email(login).password(password.toCharArray()).roles(ImmutableSet.of(RoleService.DefaultRoles.WRITER.getRole())).build();
+
+            Response response = target().path(RestUserService.USERS_PATH).request(MediaType.APPLICATION_JSON).cookie(adminCookie)
+                    .post(Entity.json(user));
+            log.debug(String.format("status: %s", response.getStatus()));
+            Assert.assertTrue(response.getStatus() == Status.CREATED.getStatusCode());
+            URI uri = response.getLocation();
+            Assert.assertNotNull(uri);
+            
+            response = target().path(RestUserService.USERS_PATH).request(MediaType.APPLICATION_JSON).cookie(adminCookie)
+                    .post(Entity.json(user));
+            log.debug(String.format("status: %s", response.getStatus()));
+            Assert.assertTrue(response.getStatus() == Status.CONFLICT.getStatusCode());
+            
+        } catch (Throwable t) {
+            log.error("testAvoidDuplicate fail", t);
+            Assert.fail();
+        }
     }
 
 }
