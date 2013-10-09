b5e70ec9148a94151d9acf1f074216daad7b2e49
Richard Louapre <richard.louapre@gmail.com>
2013-10-09 11:57:18 -0400

    Add getStatus

--- es-dms-service/src/main/java/com/github/richardwilly98/esdms/services/SimpleDocument.java
+++ es-dms-service/src/main/java/com/github/richardwilly98/esdms/services/SimpleDocument.java
@@ -32,6 +32,7 @@ import static com.google.common.collect.Sets.newHashSet;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.richardwilly98.esdms.DocumentImpl;
@@ -44,95 +45,96 @@ class SimpleDocument extends DocumentImpl {
 
     public static class Builder extends DocumentImpl.Builder {
 
-	private Document document;
+        private Document document;
 
-	public Builder document(Document document) {
-	    this.document = document;
-	    return this;
-	}
+        public Builder document(Document document) {
+            this.document = document;
+            return this;
+        }
 
-	public SimpleDocument build() {
-	    this.versions(document.getVersions()).tags(document.getTags()).ratings(document.getRatings()).id(document.getId()).name(document.getName())
-		    .disabled(document.isDisabled()).description(document.getDescription()).attributes(document.getAttributes());
-	    return new SimpleDocument(this);
-	}
+        public SimpleDocument build() {
+            this.versions(document.getVersions()).tags(document.getTags()).ratings(document.getRatings()).id(document.getId())
+                    .name(document.getName()).disabled(document.isDisabled()).description(document.getDescription())
+                    .attributes(document.getAttributes());
+            return new SimpleDocument(this);
+        }
     }
 
     SimpleDocument() {
-	this(null);
+        this(null);
     }
 
     SimpleDocument(final Builder builder) {
-	super(builder);
+        super(builder);
     }
 
     protected void setReadOnlyAttribute(String name, Object value) {
-	super.setReadOnlyAttribute(name, value);
+        super.setReadOnlyAttribute(name, value);
     }
 
     protected void removeReadOnlyAttribute(String name) {
-	setReadOnlyAttribute(name, null);
+        setReadOnlyAttribute(name, null);
     }
 
     protected void setStatus(DocumentStatus status) {
-	checkNotNull(status);
-	setReadOnlyAttribute(DocumentSystemAttributes.STATUS.getKey(), status.getStatusCode());
+        checkNotNull(status);
+        setReadOnlyAttribute(DocumentSystemAttributes.STATUS.getKey(), status.getStatusCode());
     }
 
     /*
      * addVersion set the version added to current
      */
     protected void addVersion(Version version) {
-	Version currentVersion = getCurrentVersion();
-	SimpleVersion sv = new SimpleVersion.Builder().version(version).build();
-	if (currentVersion != null) {
-	    sv.setParentId(currentVersion.getVersionId());
-	}
-	sv.setCurrent(true);
-	// Set<Version> versions = getVersions();
-	// versions.add(sv);
-	// setVersion(versions);
-	getVersions().add(sv);
+        Version currentVersion = getCurrentVersion();
+        SimpleVersion sv = new SimpleVersion.Builder().version(version).build();
+        if (currentVersion != null) {
+            sv.setParentId(currentVersion.getVersionId());
+        }
+        sv.setCurrent(true);
+        getVersions().add(sv);
     }
 
     protected void updateVersion(Version version) {
-	checkNotNull(version);
-	checkArgument(version.getVersionId() > 0);
-	// Set<Version> versions = getVersions();
-	// versions.remove(getVersion(version.getVersionId()));
-	// versions.add(version);
-	// setVersion(versions);
-	getVersions().remove(getVersion(version.getVersionId()));
-	getVersions().add(version);
+        checkNotNull(version);
+        checkArgument(version.getVersionId() > 0);
+        getVersions().remove(getVersion(version.getVersionId()));
+        getVersions().add(version);
     }
 
     protected void deleteVersion(Version version) {
-	checkNotNull(version);
-	// Set<Version> versions = getVersions();
-	// versions.remove(getVersion(version.getVersionId()));
-	// setVersion(versions);
-	getVersions().remove(getVersion(version.getVersionId()));
+        checkNotNull(version);
+        getVersions().remove(getVersion(version.getVersionId()));
     }
 
     @JsonProperty("versions")
     private Set<Version> serializeVersions() throws JsonProcessingException {
-	Set<Version> versions = getVersions();
-	if (versions == null) {
-	    return null;
-	} else {
-	    Set<Version> serializedVersions = newHashSet();
-	    for (Version version : versions) {
-		if (version.isCurrent()) {
-		    serializedVersions.add(new SimpleVersion.Builder().version(version).build());
-		} else {
-		    SimpleVersion sv = new SimpleVersion.Builder().version(version).build();
-		    sv.setCurrent(false);
-		    sv.setFile(null);
-		    serializedVersions.add(sv);
-		}
-	    }
-	    return serializedVersions;
-	}
+        Set<Version> versions = getVersions();
+        if (versions == null) {
+            return null;
+        } else {
+            Set<Version> serializedVersions = newHashSet();
+            for (Version version : versions) {
+                if (version.isCurrent()) {
+                    serializedVersions.add(new SimpleVersion.Builder().version(version).build());
+                } else {
+                    SimpleVersion sv = new SimpleVersion.Builder().version(version).build();
+                    sv.setCurrent(false);
+                    sv.setFile(null);
+                    serializedVersions.add(sv);
+                }
+            }
+            return serializedVersions;
+        }
+    }
+
+    @Override
+    @JsonIgnore
+    public DocumentStatus getStatus() {
+        DocumentStatus status = super.getStatus();
+        if (status == null) {
+            return DocumentStatus.AVAILABLE;
+        }
+        return status;
     }
 
 }
