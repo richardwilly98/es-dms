d63614fac348ab76580373527f991ab0ebbf9bd3
Richard Louapre <richard.louapre@gmail.com>
2013-10-19 15:05:28 -0400

    Code cleaning
    - Remove commented code

--- es-dms-site/yo/app/scripts/controllers/login.js
+++ es-dms-site/yo/app/scripts/controllers/login.js
@@ -1,23 +1,18 @@
 'use strict';
 
-esDmsSiteApp.controller('LoginCtrl', ['$log', '$scope', 'authenticationService', 'authService', 'sharedService', 'dialog', function ($log, $scope, authenticationService, authService, sharedService, dialog) {
+esDmsSiteApp.controller('LoginCtrl', ['$log', '$scope', 'authenticationService', 'authService', 'sharedService', 'dialog',
+  function ($log, $scope, authenticationService, authService, sharedService, dialog) {
+    $scope.login = function() {
+      $log.log('loginController - login');
+      authenticationService.login($scope.username, $scope.password, $scope.rememberMe, function(data) {
+        $log.log('data: ' + JSON.stringify(data));
+        if (data.status === 'AUTHENTICATED') {
+          dialog.close();
+        }
+      });
+    };
 
-  $scope.login = function() {
-		$log.log('loginController - login');
-		authenticationService.login($scope.username, $scope.password, $scope.rememberMe, function(data) {
-			$log.log('data: ' + data);
-			if (data.status === 'AUTHENTICATED') {
-				// authService.loginConfirmed();
-				// var token = data.token;
-				// $log.log('Authentication token: ' + token);
-				// sharedService.prepForBroadcast({logout: true});
-    //     sharedService.updateUserSettings('name', $scope.username);
-        dialog.close();
-			}
-		});
-  };
-
-  $scope.close = function () {
-    dialog.close();
-  };
-}]);
+    $scope.close = function () {
+      dialog.close();
+    };
+  }]);
--- es-dms-site/yo/app/scripts/directives/esdmsPreview.js
+++ es-dms-site/yo/app/scripts/directives/esdmsPreview.js
@@ -4,7 +4,6 @@ angular.module('esDmsSiteApp')
   .directive('esdmsPreview', ['$log', function ($log) {
     return {
       template: '<div class="dropdown">' +
-        // + '<a data-ng-click="preview(document.id)" class="dropdown-toggle">Preview</a>'
         '<a class="dropdown-toggle label"><i class="icon-white icon-picture"></i>&nbsp;Preview</a>' +
         '<div class="dropdown-menu" data-ng-include="\'views/documents/preview.html\'"></div>' +
         '</div>',
