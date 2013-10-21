f8ff51a206c0d794e8fa169b9064588e2e115f93
Richard Louapre <richard.louapre@gmail.com>
2013-10-21 08:29:27 -0400

    Use fix top navbar - add message info (when http)

--- es-dms-site/yo/app/scripts/app.js
+++ es-dms-site/yo/app/scripts/app.js
@@ -47,6 +47,56 @@ esDmsSiteApp.config(function (/*$routeProvider,*/ $stateProvider, $locationProvi
   $locationProvider.html5Mode(false);
 });
 
+esDmsSiteApp.config(function($provide, $httpProvider, $compileProvider) {
+  var elementsList = $();
+  var showMessage = function(content, cl, time) {
+    $('<div/>')
+    .addClass('message')
+    .addClass(cl)
+    .hide()
+    .fadeIn('fast')
+    .delay(time)
+    .fadeOut('fast', function() { $(this).remove(); })
+    .appendTo(elementsList)
+    .text(content);
+  };
+        
+  $httpProvider.responseInterceptors.push(function($timeout, $q) {
+    return function(promise) {
+      return promise.then(function(successResponse) {
+        if (successResponse.config.method.toUpperCase() !== 'GET') {
+          // showMessage('Success', 'successMessage', 2000);
+        }
+        return successResponse;
+      }, function(errorResponse) {
+        switch (errorResponse.status) {
+        case 401:
+          showMessage('Wrong usename or password', 'errorMessage', 20000);
+          break;
+        case 403:
+          showMessage('You don\'t have the right to do this', 'errorMessage', 20000);
+          break;
+        case 500:
+          showMessage('Server internal error: ' + errorResponse.data, 'errorMessage', 20000);
+          break;
+        default:
+          showMessage('Error ' + errorResponse.status + ': ' + errorResponse.data, 'errorMessage', 20000);
+        }
+        return $q.reject(errorResponse);
+      });
+    };
+  });
+
+  $compileProvider.directive('appMessages', function() {
+    var directiveDefinitionObject = {
+      link: function(scope, element) {
+        elementsList.push($(element));
+      }
+    };
+    return directiveDefinitionObject;
+  });
+});
+
 esDmsSiteApp.run(['$log', 'authenticationService', '$cookies', function($log, authenticationService, $cookies) {
   $log.log('*** run ***');
   var token = $cookies.ES_DMS_TICKET;
