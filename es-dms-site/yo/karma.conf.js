d1bd7c71652aae22fbc42e431c3f20b0af77f907
Richard Louapre <richard.louapre@gmail.com>
2013-11-25 10:49:27 -0500

    Update Karma

--- es-dms-site/yo/karma.conf.js
+++ es-dms-site/yo/karma.conf.js
@@ -1,68 +1,87 @@
 // Karma configuration
+// Generated on Sat Nov 23 2013 06:35:44 GMT-0500 (EST)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/bower_components/angular/angular.js',
-  'app/bower_components/jquery/jquery.js',
-  'app/bower_components/angular-cookies/angular-cookies.js',
-  'app/bower_components/angular-sanitize/angular-sanitize.js',
-  'app/bower_components/angular-mocks/angular-mocks.js',
-  'app/bower_components/angular-resource/angular-resource.js',
-  'app/bower_components/angular-bootstrap/ui-bootstrap.js',
-  'app/bower_components/angular-ui-router/release/angular-ui-router.js',
-  'app/bower_components/angular-authentication/js/angular-authentication.js',
-  'app/bower_components/angular-http-auth/src/angular-http-auth.js',
-  'app/bower_components/ng-tags-input/ng-tags-input.js',
-  'app/bower_components/lodash/dist/lodash.js',
-  'app/bower_components/angularjs-file-upload/angular-file-upload.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  //'test/mock/**/*.js',
-  'test/spec/**/*.js'
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [];
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-// web server port
-port = 18080;
 
-// cli runner port
-runnerPort = 9100;
+    // list of files / patterns to load in the browser
+    files: [
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/jquery/jquery.js',
+      'app/bower_components/angular-cookies/angular-cookies.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap.js',
+      'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'app/bower_components/angular-authentication/js/angular-authentication.js',
+      'app/bower_components/angular-http-auth/src/angular-http-auth.js',
+      'app/bower_components/ng-tags-input/ng-tags-input.js',
+      'app/bower_components/lodash/dist/lodash.js',
+      'app/bower_components/angularjs-file-upload/angular-file-upload.js',
+      'app/scripts/*.js',
+      'app/scripts/**/*.js',
+      //'test/mock/**/*.js',
+      'test/spec/**/*.js'
+    ],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-//browsers = ['Chrome'];
-browsers = ['PhantomJS'];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // web server port
+    port: 9876,
+
+    // cli runner port
+    runnerPort: 9100,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    //browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
